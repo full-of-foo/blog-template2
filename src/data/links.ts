2c3c5d6a65d89e3eff0f6c88db8927ec340268d4
Tony Troy <lightweightdevelopment@gmail.com>
2025-02-09 17:50:44 +0000

    Added updated social links + added LinkedIn social; Rename Show All to Blogs; Update title and subtitle

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,24 @@
 import TwitterIcon from '@/components/icons/TwitterIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
+import LinkedInIcon from '@/components/icons/LinkedInIcon'
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
 		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://github.com/full-of-foo',
 		icon: GithubIcon
 	},
 
 	{
 		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://x.com/devtoeknee',
 		icon: TwitterIcon
+	},
+
+	{
+		name: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/anthony-troy',
+		icon: LinkedInIcon
 	}
 ] as const
